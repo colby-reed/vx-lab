94a2892f2c242747a35c2dead08ff4288aeb1517
Justin Betz <jbetz@cumulusnetworks.com>
2019-11-27 20:15:30 +0000

    ci-common changes

--- tests/tests-netq-ts-inside.sh
+++ tests/tests-netq-ts-inside.sh
@@ -6,8 +6,6 @@ if [ "$?" != "0" ]; then
     exit 1
 fi
 }
-
-set -e
 set -x
 
 echo "netq show agents"
@@ -16,20 +14,41 @@ check_state
 
 echo "netq check bgp"
 netq check bgp
-#netq check bgp | grep -q "Failed Nodes: 0"
 check_state
-#netq check bgp | grep -q "Failed Sessions: 0"
+netq check bgp include 0
+check_state
+netq check bgp include 1
+check_state
+netq check bgp include 2
 check_state
 
 echo "netq check vxlan"
 netq check vxlan
-#netq check vxlan | grep -q "Failed Nodes: 0"
+check_state
+netq check vxlan include 0
+check_state
+netq check vxlan include 1
 check_state
 
 echo "netq check evpn"
 netq check evpn
-#netq check evpn | grep -q "Failed Nodes: 0"
+check_state
+netq check evpn include 0
+check_state
+netq check evpn include 1
+check_state
+netq check evpn include 2
+check_state
+netq check evpn include 3
+check_state
+netq check evpn include 4
+check_state
+netq check evpn include 5
+check_state
+netq check evpn include 6
+check_state
+netq check evpn include 7 
 check_state
 
+#Netq traces between some points in the topology
 
-#Netq traces between some points in the topology?
