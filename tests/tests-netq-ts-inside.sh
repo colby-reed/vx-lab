2392656c1ea66e3e12677e4849371de54e49c9ca
Justin Betz <jbetz@cumulusnetworks.com>
2020-05-08 04:53:19 +0000

    crank up more testing in ci

--- tests/tests-netq-ts-inside.sh
+++ tests/tests-netq-ts-inside.sh
@@ -16,8 +16,8 @@ netq show agents
 check_state
 
 echo "netq check bgp"
-netq check bgp
-check_state
+#netq check bgp
+#check_state
 netq check bgp include 0
 check_state
 netq check bgp include 1
@@ -26,16 +26,16 @@ netq check bgp include 2
 check_state
 
 echo "netq check vxlan"
-netq check vxlan
-check_state
+#netq check vxlan
+#check_state
 netq check vxlan include 0
 check_state
 netq check vxlan include 1
 check_state
 
 echo "netq check evpn"
-netq check evpn
-check_state
+#netq check evpn
+#check_state
 netq check evpn include 0
 check_state
 netq check evpn include 1
@@ -53,5 +53,49 @@ check_state
 netq check evpn include 7 
 check_state
 
-#Netq traces between some points in the topology
+echo "netq check clag"
+netq check clag include 0
+check_state
+netq check clag include 1
+check_state
+netq check clag include 2
+check_state
+netq check clag include 3
+check_state
+netq check clag include 4
+check_state
+netq check clag include 5
+check_state
+netq check clag include 6
+check_state
+netq check clag include 7
+check_state
+netq check clag include 8
+check_state
+netq check clag include 9
+check_state
+netq check clag include 10
+check_state
+
+echo "netq check cl-version"
+netq check cl-version include 0
+check_state
+
+echo "netq check mtu"
+#netq check mtu 0
+#check_state
+netq check mtu 1
+check_state
+netq check mtu 2
+check_state
+
+echo "netq check ntp"
+netq check ntp 0
+check_state
+
+echo "netq check vlan"
+netq check vlan include 0
+check_state
+netq check vlan include 1
+check_state
 
